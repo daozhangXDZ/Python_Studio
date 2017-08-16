f70de1fcaa6cb021a0a9dd40dc9545008fa12124
DaoZhang <DaoZhang_XDZ@163.com>
2017-08-16 23:50:37 +0800

    Exam

--- Exam/1/.idea/script/webExample/urllib_1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: GBK -*-
-import urllib.request
-response1= urllib.request.urlopen("http://www.baidu.com");
-strweb1= response1.read();
--- Exam/1/.idea/script/webExample/urllib_3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: GBK -*-
-import urllib.request;
-import urllib.response;
-values = {"username": "1016903103@qq.com", "password": "XXXX"}
-url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
-response = urllib.request.urlopen(url);
-print(response.read());
